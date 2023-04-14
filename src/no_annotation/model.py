5b90c128b5e474777432a678a5ec32d1de65a858
AMKCode <amk@DESKTOP-G9D4VT6.localdomain>
2023-04-14 08:45:39 -0400

    generated heatmap and boxes

--- src/no_annotation/model.py
+++ src/no_annotation/model.py
@@ -12,78 +12,61 @@ class CustomVGG16(nn.Module):
         first_conv_layer = [nn.Conv2d(1, 3, kernel_size=3, stride=1, padding=1, dilation=1, groups=1, bias=True)]
         first_conv_layer.extend(list(self.feature_extractor.features))  
         self.feature_extractor.features= nn.Sequential(*first_conv_layer )  
-
-        # convert to greyscale input
-        # self.feature_extractor.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False, input_shape=(224, 224, 1))
+        self.feature_extractor = self.feature_extractor.features[:-1]
         
-        self.classification_head = nn.Linear(1000, n_classes) # since the number of output for VGG16 is 1000
+        self.classification_head = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(
+                kernel_size=(INPUT_IMG_SIZE[0] // 2 ** 5, INPUT_IMG_SIZE[1] // 2 ** 5)
+            ),
+            nn.Flatten(),
+            nn.Linear(
+                in_features=self.feature_extractor[-2].out_channels,
+                out_features=n_classes,
+            ),
+        )
+        self._freeze_params()
 
+    def _freeze_params(self):
+        for param in self.feature_extractor[:23].parameters():
+            param.requires_grad = False
+        
 
     def forward(self, x):
         feature_maps = self.feature_extractor(x)
         scores = self.classification_head(feature_maps)
-        return scores
+        feature_maps = self.feature_extractor(x)
+        scores = self.classification_head(feature_maps)
 
+        if self.training:
+            return scores
 
-# class CustomVGG16(nn.Module):
-#     def __init__(self, n_classes=2, pretrained=True):
-#         super(CustomVGG16, self).__init__()
-#         self.feature_extractor = models.vgg16(pretrained=pretrained).features[:1]
+        else:
+            probs = nn.functional.softmax(scores, dim=-1)
 
-#         # convert to greyscale input
-#         self.feature_extractor.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        
-#         self.classification_head = nn.Sequential(
-#             nn.MaxPool2d(kernel_size=2, stride=2),
-#             nn.AvgPool2d(
-#                 kernel_size=(INPUT_IMG_SIZE[0] // 2 ** 5, INPUT_IMG_SIZE[1] // 2 ** 5)
-#             ),
-#             nn.Flatten(),
-#             nn.Linear(
-#                 in_features=self.feature_extractor[-2].out_channels,
-#                 out_features=n_classes,
-#             ),
-#         )
-#         self._freeze_params()
+            weights = self.classification_head[3].weight
+            weights = (
+                weights.unsqueeze(-1)
+                .unsqueeze(-1)
+                .unsqueeze(0)
+                .repeat(
+                    (
+                        x.size(0),
+                        1,
+                        1,
+                        INPUT_IMG_SIZE[0] // 2 ** 4,
+                        INPUT_IMG_SIZE[0] // 2 ** 4,
+                    )
+                )
+            )
+            feature_maps = feature_maps.unsqueeze(1).repeat((1, probs.size(1), 1, 1, 1))
+            location = torch.mul(weights, feature_maps).sum(axis=2)
+            location = F.interpolate(location, size=INPUT_IMG_SIZE, mode="bilinear")
 
-#     def _freeze_params(self):
-#         for param in self.feature_extractor[:23].parameters():
-#             param.requires_grad = False
+            maxs, _ = location.max(dim=-1, keepdim=True)
+            maxs, _ = maxs.max(dim=-2, keepdim=True)
+            mins, _ = location.min(dim=-1, keepdim=True)
+            mins, _ = mins.min(dim=-2, keepdim=True)
+            norm_location = (location - mins) / (maxs - mins)
 
-#     def forward(self, x):
-#         feature_maps = self.feature_extractor(x)
-#         scores = self.classification_head(feature_maps)
-
-#         if self.training:
-#             return scores
-
-#         else:
-#             probs = nn.functional.softmax(scores, dim=-1)
-
-#             weights = self.classification_head[3].weight
-#             weights = (
-#                 weights.unsqueeze(-1)
-#                 .unsqueeze(-1)
-#                 .unsqueeze(0)
-#                 .repeat(
-#                     (
-#                         x.size(0),
-#                         1,
-#                         1,
-#                         INPUT_IMG_SIZE[0] // 2 ** 4,
-#                         INPUT_IMG_SIZE[0] // 2 ** 4,
-#                     )
-#                 )
-#             )
-#             feature_maps = feature_maps.unsqueeze(1).repeat((1, probs.size(1), 1, 1, 1))
-#             location = torch.mul(weights, feature_maps).sum(axis=2)
-#             location = F.interpolate(location, size=INPUT_IMG_SIZE, mode="bilinear")
-
-#             maxs, _ = location.max(dim=-1, keepdim=True)
-#             maxs, _ = maxs.max(dim=-2, keepdim=True)
-#             mins, _ = location.min(dim=-1, keepdim=True)
-#             mins, _ = mins.min(dim=-2, keepdim=True)
-#             norm_location = (location - mins) / (maxs - mins)
-
-#             return probs, norm_location
-
+            return probs, norm_location
--- src/no_annotation/utils.py
+++ src/no_annotation/utils.py
@@ -1,9 +1,12 @@
 import torch
 from torchvision import transforms
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from sklearn.metrics import confusion_matrix, accuracy_score, balanced_accuracy_score
+
+NEG_CLASS = 1
 
 def train(
     dataloader, model, optimizer, criterion, epochs, device, target_accuracy=None
@@ -64,7 +67,7 @@ def evaluate(model, dataloader, device):
         inputs = inputs.to(device)
         labels = labels.to(device)
 
-        preds_probs = model(inputs)
+        preds_probs = model(inputs)[0]
         preds_class = torch.argmax(preds_probs, dim=-1)
 
         labels = labels.to("cpu").numpy()
@@ -78,4 +81,105 @@ def evaluate(model, dataloader, device):
 
     print("Accuracy: {:.4f}".format(accuracy))
     print("Balanced Accuracy: {:.4f}".format(balanced_accuracy))
+    print()
+    plot_confusion_matrix(y_true, y_pred, class_names=class_names)
 
+def plot_confusion_matrix(y_true, y_pred, class_names="auto"):
+    confusion = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=[5, 5])
+    
+    sns.heatmap(
+        confusion,
+        annot=True,
+        cbar=False,
+        xticklabels=class_names,
+        yticklabels=class_names,
+    )
+
+    plt.ylabel("True labels")
+    plt.xlabel("Predicted labels")
+    plt.title("Confusion Matrix")
+    plt.show()
+    
+    
+def get_bbox_from_heatmap(heatmap, thres=0.8):
+    """
+    Returns bounding box around the defected area:
+    Upper left and lower right corner.
+    
+    Threshold affects size of the bounding box.
+    The higher the threshold, the wider the bounding box.
+    """
+    binary_map = heatmap > thres
+
+    x_dim = np.max(binary_map, axis=0) * np.arange(0, binary_map.shape[1])
+    x_0 = int(x_dim[x_dim > 0].min())
+    x_1 = int(x_dim.max())
+
+    y_dim = np.max(binary_map, axis=1) * np.arange(0, binary_map.shape[0])
+    y_0 = int(y_dim[y_dim > 0].min())
+    y_1 = int(y_dim.max())
+
+    return x_0, y_0, x_1, y_1
+
+
+def predict_localize(
+    model, dataloader, device, thres=0.8, n_samples=9, show_heatmap=False
+):
+    """
+    Runs predictions for the samples in the dataloader.
+    Shows image, its true label, predicted label and probability.
+    If an anomaly is predicted, draws bbox around defected region and heatmap.
+    """
+    model.to(device)
+    model.eval()
+
+    class_names = ['non-defective', 'defective']
+    transform_to_PIL = transforms.ToPILImage()
+
+    n_cols = 3
+    n_rows = int(np.ceil(n_samples / n_cols))
+    plt.figure(figsize=[n_cols * 5, n_rows * 5])
+
+    counter = 0
+    for inputs, labels in dataloader:
+        inputs = inputs.to(device)
+        out = model(inputs)
+        probs, class_preds = torch.max(out[0], dim=-1)
+        feature_maps = out[1].to("cpu")
+
+        for img_i in range(inputs.size(0)):
+            img = transform_to_PIL(inputs[img_i])
+            class_pred = class_preds[img_i]
+            prob = probs[img_i]
+            label = labels[img_i]
+            heatmap = feature_maps[img_i][NEG_CLASS].detach().numpy()
+
+            counter += 1
+            plt.subplot(n_rows, n_cols, counter)
+            plt.imshow(img)
+            plt.axis("off")
+            plt.title(
+                "Predicted: {}, Prob: {:.3f}, True Label: {}".format(
+                    class_names[class_pred], prob, class_names[label]
+                )
+            )
+
+            if class_pred == NEG_CLASS:
+                x_0, y_0, x_1, y_1 = get_bbox_from_heatmap(heatmap, thres)
+                rectangle = Rectangle(
+                    (x_0, y_0),
+                    x_1 - x_0,
+                    y_1 - y_0,
+                    edgecolor="red",
+                    facecolor="none",
+                    lw=3,
+                )
+                plt.gca().add_patch(rectangle)
+                if show_heatmap:
+                    plt.imshow(heatmap, cmap="Reds", alpha=0.3)
+
+            if counter == n_samples:
+                plt.tight_layout()
+                plt.show()
+                return
