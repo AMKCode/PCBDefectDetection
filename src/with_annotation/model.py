c4d3629a3a4449b94d18ce9f137182cb90b1b0ab
AMKCode <amk@DESKTOP-G9D4VT6.localdomain>
2023-04-29 16:04:43 -0400

    trained RCNN

--- src/with_annotation/model.py
+++ src/with_annotation/model.py
@@ -11,11 +11,11 @@ class CustomRCNN(nn.Module):
         super(CustomRCNN, self).__init__()
         self.model = models.detection.fasterrcnn_resnet50_fpn(pretrained=pretrained)
         self.model.backbone.body.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        self.model.transform = GeneralizedRCNNTransform(min_size=(800,), max_size=1333, image_mean=(0.5,), image_std=(0.5,))
+        self.model.transform = GeneralizedRCNNTransform(min_size=(640,), max_size=1333, image_mean=(0.5,), image_std=(0.5,))
         # get number of input features for the classifier
         in_features = self.model.roi_heads.box_predictor.cls_score.in_features
 
         # replace pre-trained head with new one
         self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, n_classes)
-    def forward(self, image, target):
-        self.model.forward(image, target)
+    def forward(self, image, target=None):
+        return self.model.forward(image, target)
